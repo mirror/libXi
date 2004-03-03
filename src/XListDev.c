e1cf8e198c35b65c95d54a5dfe888e054f4b7056
Egbert Eich <eich@suse.de>
2004-03-03 12:11:28 +0000

    Importing vendor version xf86-4_4_0 on Wed Mar 3 04:09:24 PST 2004

--- src/XListDev.c
+++ src/XListDev.c
@@ -45,7 +45,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 SOFTWARE.
 
 ********************************************************/
-/* $XFree86: xc/lib/Xi/XListDev.c,v 3.5 2002/10/16 00:37:29 dawes Exp $ */
+/* $XFree86: xc/lib/Xi/XListDev.c,v 3.4 2001/12/14 19:55:19 dawes Exp $ */
 
 /***********************************************************************
  *
